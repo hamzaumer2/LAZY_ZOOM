fffc1cf03d5425cd0762a957ed4d35c63e8b4aa4
Hamza Umer <71342062+hamzaumer2@users.noreply.github.com>
2022-05-23 21:44:22 +0500

    Update Lazy_Zoom.py

--- Lazy_Zoom.py
+++ Lazy_Zoom.py
@@ -1,4 +1,3 @@
-import shutil
 import webbrowser
 import time
 import os
@@ -11,65 +10,28 @@ payment = ""
 print("***************************     LAZY CLASSES     ***************************")
 print("**********************     DEVELOPED BY HAMZA UMER    **********************")
 
-cont = str(input("THIS PROGRAM IS PASSWORD PROTECTED: "
-                 "DAMAGE. CONTINUE? (Y/n)"))
+classes = int(input("How Many Classes do you have today?"))
+done = 0
+joiined = 0
+link_arr = []
+time_arr = []
 
-if cont != "Y" or cont != "y":
-    if cont != "N" or cont != "n":
-        print("INCORRECT ENTRY!!!")
-    print("EXITING PROGRAM!!!")
-    os.system("pause")
-    exit()
+while done < classes:
+    link = input("Enter Zoom Link Number in ORDER of TIME TABLE: ")
+    link_arr.append(link)
+    time_of_join = input("Enter Time (Format:- HH:MM:SS)(24 hour format) ->")
+    time_arr.append(time_of_join)
+    done = done + 1
 
-while payment != "PASSWORD":
-    payment = input("ENTER PASSWORD PROVIDED TO YOU TO UNLOCK THE FILE: ")
+Current_time = time.strftime("%H:%M:%S")
 
-    if payment != "PASSWORD":
-        attempts = attempts - 1
-        attempts_show = str(attempts)
-        print("INCORRECT PASSWORD... RETRY... ATTEMPTS REMAIN:  " + attempts_show)
-        if attempts == 0:
-            print("INCORRECT PASSWORD..... Unauthorised access..... Deleting Program from Your Computer")
-            path = os.getcwd()
-            print("DELETING FILES FROM: " + path)
-            print("RESTARTING DEVICE IN 10 SECONDS!!!")
-            os.system("shutdown /s /t 10")
-            print("DELETING THE PROGRAM!!!")
-            time.sleep(5)
-            os.remove("Lazy_Zoom.exe")
-            os.system("pause")
-            exit()
+while joined < classes:
+    while Current_time != time_arr[joined]:
+        print("Current Time is: " + Current_time + "\t\tNext Class is at: " + time_arr[joined] + "\t\tLink is: " + link_arr[joined])
+        Current_time = time.strftime("%H:%M:%S")
+        time.sleep(1)
 
-
-
-
-
-
-else:
-    print("AUTHORIZED!!!")
-    classes = int(input("How Many Classes do you have today?"))
-    done = 0
-    joined = 0
-    link_arr = []
-    time_arr = []
-
-    while done < classes:
-        link = input("Enter Zoom Link Number in ORDER of TIME TABLE: ")
-        link_arr.append(link)
-        time_of_join = input("Enter Time (Format:- HH:MM:SS)(24 hour format) ->")
-        time_arr.append(time_of_join)
-        done = done + 1
-
-    Current_time = time.strftime("%H:%M:%S")
-
-    while joined < classes:
-        while Current_time != time_arr[joined]:
-            print("Current Time is: " + Current_time + "\t\tNext Class is at: " + time_arr[joined] + "\t\tLink is: " +
-                  link_arr[joined])
-            Current_time = time.strftime("%H:%M:%S")
-            time.sleep(1)
-
-        if Current_time == time_arr[joined]:
-            print("JOINING CLASS :D")
-            webbrowser.open(link_arr[joined])
-            joined = joined + 1
+    if Current_time == time_arr[joined]:
+        print("JOINING CLASS :D")
+        webbrowser.open(link_arr[joined])
+        joined = joined + 1
